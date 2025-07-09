49fc8840bb9a73ee74b12c99acd8db27d0167cfc
riswandaP <riswandaputra03@gmail.com>
2025-07-09 14:22:04 +0700

    akan dieksekusi karena array data hanya memiliki 3 elemen

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -14,4 +14,10 @@ int main()
         cout << data.at(5) << endl;
         //memanggil array elemen ke 5
     }
+
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        //akan dieksekusi karena array data hanya memiliki 3 elemen
+    }
 }
