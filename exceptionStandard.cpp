3d8ef823f42fd21abeb48045db879710d12fda36
riswandaP <riswandaputra03@gmail.com>
2025-07-09 14:20:36 +0700

    pesan array integer 3 elemen

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -5,3 +5,11 @@
 //untuk obyek array yang akan kita gunakan
 using namespace std;
 
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = {3, 5, 7};
+        //pesan array integer 3 elemen
+    }
+}
