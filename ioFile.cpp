92f702c75a5d3974b6d702c5f3df4270c050dc3a
riswandaP <riswandaputra03@gmail.com>
2025-07-09 14:25:27 +0700

    selesai dalam menulis sekarang tutup filenya

--- ioFile.cpp
+++ ioFile.cpp
@@ -24,3 +24,7 @@ int main()
         //menulis dan memasukan nilai dari 'baris' kedalam file
         outfile << baris << endl;
     }
+
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
