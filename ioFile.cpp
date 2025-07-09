5741bdf5ff76f2925e9e06dde6498ba9d4500686
riswandaP <riswandaputra03@gmail.com>
2025-07-09 14:26:21 +0700

    membuka file dalam mode membaca dan menunjuk ke sebuah file

--- ioFile.cpp
+++ ioFile.cpp
@@ -28,3 +28,11 @@ int main()
     //selesai dalam menulis sekarang tutup filenya
     outfile.close();
 
+    //membuka file dalam mode membaca 
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+
+}
