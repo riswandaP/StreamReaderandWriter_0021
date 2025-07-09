7e7072e4f7fbcef20f936ce1fa92e865096f3194
riswandaP <riswandaputra03@gmail.com>
2025-07-09 14:33:59 +0700

    menunjukan ke sebuah nama file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -12,3 +12,8 @@ int main() {
 
 	// membuka file dalam mode menulis.
 	ofstream outfile;
+	// menunjukan ke sebuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q'\ untuk keluar" << endl;
+
