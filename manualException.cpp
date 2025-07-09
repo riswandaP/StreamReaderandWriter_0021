24cecd1a7d9e06f9d0c512119405b67203264591
riswandaP <riswandaputra03@gmail.com>
2025-07-09 14:39:12 +0700

    melempar sebuah integer

--- manualException.cpp
+++ manualException.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; // melemparkan sebuah interger
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+
+}
