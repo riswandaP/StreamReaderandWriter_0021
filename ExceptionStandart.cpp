78675334ef5dec8a0535044f25ed177d2a071025
riswandaP <riswandaputra03@gmail.com>
2026-06-02 20:13:03 +0700

    Membuat penanda 1

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -9,18 +9,6 @@ int main()
     cout << "Awal program" << endl; //penanda 1
     try
     {   
-        //deklarai array data dengan panjang index 3
-        array <int, 3> data = { 1, 2, 3};
-        //memanggil array element ke-5 yang berarti akan terjadi error
-        cout << data.at(5) << endl;
-    }
-    //kondisi jika terjadi error didalam try
-    catch(exception& e)
-    {
-        
-        cout << e.what() << endl;
-    }
-    cout << "Baris program terakhir" << endl; //penanda 2
-    return 0;
+
 
 }
