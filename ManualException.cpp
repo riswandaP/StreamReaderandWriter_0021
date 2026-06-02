4e85826dd45d96c30b08c7c1a7312277e623e014
riswandaP <riswandaputra03@gmail.com>
2026-06-02 20:24:34 +0700

    Membuat isi dari try pengujian error

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        //throw 0.5; //melempar sebuah integer maka
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+
+}
